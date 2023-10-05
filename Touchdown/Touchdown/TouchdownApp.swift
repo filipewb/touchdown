bc3d7db8b731dcd2853a7faf8096d0c887b0c951
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-10-05 17:51:22 -0300

    feat
    first commit

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Filipe Boeck on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
