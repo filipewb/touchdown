eaaa8d45a8f205597672308ca88d04f08b3d8bfc
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-10-05 19:02:37 -0300

    feat:
    criacao da navigationBar

---
+++ Touchdown/Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,53 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Filipe Boeck on 05/10/23.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    @State private var isAnimated: Bool = false
+    
+    var body: some View {
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundStyle(.black)
+            })
+            
+            Spacer()
+            
+            LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
+             
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                    .foregroundStyle(.black)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
+            })
+        }
+    }
+}
+
+#Preview {
+    NavigationBarView()
+        .previewLayout(.sizeThatFits)
+}
