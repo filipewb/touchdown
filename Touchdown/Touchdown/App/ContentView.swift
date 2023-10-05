42e77de88d033c0fca09d2ccffe49f9a920a4aef
Filipe Wolf Boeck <85641580+filipewb@users.noreply.github.com>
2023-10-05 19:03:36 -0300

    refactor:
    refatoracao feita na contentView

--- Touchdown/Touchdown/App/ContentView.swift
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -10,8 +10,23 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.safeAreaInsets?.top)
+                    .background(Color.white)
+                    .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
